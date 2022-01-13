0f8fdb8fb6bc82a53447a9356a97f1f28e399674
claudiocassimiro <days_surfing@live.com>
2022-01-13 19:44:13 -0300

    Cria script

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector.apply('canvas');
+const context = canvas.getContext('2d');
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    }
+  }
+}
