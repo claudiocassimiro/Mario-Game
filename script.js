f4804805ecaee67481c042aff5d158cff469fbab
claudiocassimiro <days_surfing@live.com>
2022-01-14 14:04:28 -0300

    Adiciona metodo draw e instancia o objeto player

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ class Player {
       x: 100,
       y: 100,
     }
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    context.fillRect(
+      this.position.x, this.position.y, this.width, this.height
+      );
   }
 }
+
+const player = new Player();
+player.draw();
